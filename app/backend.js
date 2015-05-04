6c83d0090b36fdb49151b6d50a3455b2f62f6879
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-05-04 02:09:25 +0200

    fix require socket.io

--- app/backend.js
+++ app/backend.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {path: '/mean/socket.io'});
 
 app.get('/mean', function(req, res){
   res.sendFile(__dirname + '/index.html');
