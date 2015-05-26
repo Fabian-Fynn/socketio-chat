9b807758e0bba7d286c01799d10054de00ff992c
Fabian Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-05-26 11:51:19 +0200

    Update backend.js path

--- app/backend.js
+++ app/backend.js
@@ -1,8 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, {path: '/mean/socket.io'});
+var io = require('socket.io')(http, {path: '/docker-mean/socket.io'});
 
-app.get('/mean', function(req, res){
+app.get('/docker-mean', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
